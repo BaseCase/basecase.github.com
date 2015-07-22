d5b88d402b4d40f84214607b26f3abb8860e7396
Casey Brant <casey@caseybrant.com>
2015-07-22 17:17:10 -0500

    Update map.js

--- map.js
+++ map.js
@@ -2,7 +2,7 @@
 
 function make_that_map() {
   var start_pos = new google.maps.LatLng('37.6889102', '-122.4706087');
-  var cur_pos = new google.maps.LatLng('41.105097','-100.765228');
+  var cur_pos = new google.maps.LatLng('40.814419','-96.789003');
   var dest_pos = new google.maps.LatLng('43.0849935', '-89.4064204');
 
   var map = new google.maps.Map(document.getElementById("map"), {
