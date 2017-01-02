45d0d9405574ace8c668603693f4123bd87dfd9a
Casey Brant <casey@caseybrant.com>
2017-01-02 11:50:39 -0600

    sets down the basis for 2017 media consumption list
    this time with data and templates instead of hard-coded html!

---
+++ movies-books-games/src/build.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'yaml'
+
+class MakeThatPage
+  def initialize
+    @template = ERB.new(File.open('src/template.html.erb').read)
+    @entries = YAML.load_file('src/mbg_data.yml')
+  end
+
+  def render
+    @template.result(binding)
+  end
+end
+
+maker = MakeThatPage.new
+puts maker.render
