2b50ab5e6cd40a8140d8770b7837665149037e35
Casey Brant <casey@caseybrant.com>
2017-01-10 14:39:23 -0500

    finalize build script and data format for mbg page

--- movies-books-games/src/build.rb
+++ movies-books-games/src/build.rb
@@ -6,11 +6,18 @@ require 'yaml'
 class MakeThatPage
   def initialize
     @template = ERB.new(File.open('src/template.html.erb').read)
-    @entries_by_month = YAML.load_file('src/mbg_data.yml')
+    @entries_by_month = entries_by_month
   end
 
   def render
     @template.result(binding)
+  end
+
+  private
+
+  def entries_by_month
+    entries = YAML.load_file('src/mbg_data.yml')
+    grouped = entries.group_by { |entry| entry['completion_date'].month }
   end
 end
 
